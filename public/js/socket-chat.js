70060f88bfddac4c0930f35af56b8ca4b9aa510b
Ezequiel <ezequielsobrino@gmail.com>
2018-11-05 00:23:34 -0300

    Chat finalizado

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -1,6 +1,7 @@
 var socket = io();
 
 var params = new URLSearchParams(window.location.search);
+
 if (!params.has('nombre') || !params.has('sala')) {
     window.location = 'index.html';
     throw new Error('El nombre y sala son necesarios');
@@ -11,12 +12,15 @@ var usuario = {
     sala: params.get('sala')
 };
 
+
+
 socket.on('connect', function() {
     console.log('Conectado al servidor');
 
     socket.emit('entrarChat', usuario, function(resp) {
-        console.log('Usuarios conectados ', resp);
+        renderizarUsuarios(resp);
     });
+
 });
 
 // escuchar
@@ -26,30 +30,19 @@ socket.on('disconnect', function() {
 
 });
 
-
-// Enviar información
-// socket.emit('crearMensaje', {
-//     usuario: 'Fernando',
-//     mensaje: 'Hola Mundo'
-// }, function(resp) {
-//     console.log('respuesta server: ', resp);
-// });
-
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-
-    console.log('Servidor:', mensaje);
+    renderizarMensajes(mensaje, false);
+    scrollBottom();
 });
 
 // Escuchar cambios de usuarios
-// Usuario entra o sale del chat
-
+// cuando un usuario entra o sale del chat
 socket.on('listaPersona', function(personas) {
-
-    console.log(personas);
+    renderizarUsuarios(personas);
 });
 
 // Mensajes privados
 socket.on('mensajePrivado', function(mensaje) {
-    console.log('Mensaje privado: ', mensaje);
+    console.log('Mensaje Privado:', mensaje);
 });
